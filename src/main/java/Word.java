5cd15d55885bfb75d901027312b8681e049f12b7
vladyulik <vanya123wow321@gmail.com>
2024-11-19 04:35:00 +0200

    Add documentation for Word class

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,10 +1,28 @@
+/**
+ * Represents a word as an array of {@code Letter} objects and provides methods
+ * to work with words. A {@code Word} can be initialized with an array of
+ * {@code Letter} objects or a {@code StringBuffer}.
+ * Implements {@code Comparable} to allow comparison based on the count of vowels.
+ */
 public class Word implements Comparable<Word> {
     private Letter[] value;
 
+    /**
+     * Constructs a {@code Word} object with the specified array of {@code Letter} objects.
+     *
+     * @param word an array of {@code Letter} objects representing the word
+     */
     public Word(Letter[] word) {
         this.value = word;
     }
 
+    /**
+     * Constructs a {@code Word} object using characters from the specified
+     * {@code StringBuffer}. Each character in the {@code StringBuffer} is
+     * converted to a {@code Letter} object.
+     *
+     * @param sb the {@code StringBuffer} whose characters form the {@code Word}
+     */
     public Word(StringBuffer sb) {
         this.value = new Letter[sb.length()];
         for (int i = 0; i < sb.length(); i++) {
@@ -12,10 +30,20 @@ public class Word implements Comparable<Word> {
         }
     }
 
+    /**
+     * Returns the array of {@code Letter} objects that represents this {@code Word}.
+     *
+     * @return the array of {@code Letter} objects in this {@code Word}
+     */
     public Letter[] getValue() {
         return this.value;
     }
 
+    /**
+     * Counts the number of vowels in this {@code Word}.
+     *
+     * @return the number of vowels in this {@code Word}
+     */
     public int countVowels() {
         int total = 0;
         for (Letter letter : this.value) {
@@ -26,6 +54,14 @@ public class Word implements Comparable<Word> {
         return total;
     }
 
+    /**
+     * Compares this {@code Word} with another {@code Word} based on the count
+     * of vowels. Words with more vowels are considered greater.
+     *
+     * @param other the {@code Word} to be compared
+     * @return a positive integer if this {@code Word} has more vowels than {@code other};
+     *         a negative integer if it has fewer vowels; 0 if the vowel counts are equal
+     */
     @Override
     public int compareTo(Word other) {
         if (this.countVowels() > other.countVowels()) { return 1; }
@@ -33,6 +69,12 @@ public class Word implements Comparable<Word> {
         return 0;
     }
 
+    /**
+     * Returns a string representation of this {@code Word}.
+     *
+     * @return the string formed by concatenating the string representations
+     *         of each {@code Letter} in this {@code Word}
+     */
     @Override
     public String toString() {
         StringBuffer sb = new StringBuffer();
