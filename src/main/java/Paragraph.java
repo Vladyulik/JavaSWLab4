b4160d1c48ab9dd7623f473fe43e1793458276e3
vladyulik <vanya123wow321@gmail.com>
2024-11-19 04:59:12 +0200

    Add documentation for Paragraph class

--- src/main/java/Paragraph.java
+++ src/main/java/Paragraph.java
@@ -1,14 +1,37 @@
+/**
+ * Represents a paragraph as an array of {@code Sentence} objects.
+ * A {@code Paragraph} can be initialized with an array of {@code Sentence} objects
+ * or parsed from a {@code StringBuffer}.
+ *
+ * <p>
+ * The paragraph separates and organizes sentences based on sentence-ending
+ * punctuation marks such as '.', '!', and '?'.
+ * </p>
+ */
 public class Paragraph {
     private Sentence[] value;
 
+    /**
+     * Constructs a {@code Paragraph} object using the specified array of {@code Sentence} objects.
+     *
+     * @param paragraph an array of {@code Sentence} objects representing the paragraph
+     */
     public Paragraph(Sentence[] paragraph) {
         this.value = paragraph;
     }
 
+    /**
+     * Constructs a {@code Paragraph} object by parsing the provided {@code StringBuffer}.
+     * Sentences are identified and separated based on the sentence-ending punctuation
+     * marks '.', '!', and '?'.
+     *
+     * @param sb the {@code StringBuffer} containing the paragraph to parse
+     */
     public Paragraph(StringBuffer sb) {
         StringBuffer current = new StringBuffer();
         int sentenceCount = 0;
 
+        // Determine the number of sentences
         for (int i = 0; i < sb.length(); i++) {
             char ch = sb.charAt(i);
             current.append(ch);
@@ -25,6 +48,7 @@ public class Paragraph {
         current.setLength(0);
         int sentenceIndex = 0;
 
+        // Populate the paragraph with Sentence objects
         for (int i = 0; i < sb.length(); i++) {
             char ch = sb.charAt(i);
             current.append(ch);
@@ -38,10 +62,21 @@ public class Paragraph {
         }
     }
 
+    /**
+     * Returns the array of {@code Sentence} objects that make up this {@code Paragraph}.
+     *
+     * @return an array of {@code Sentence} objects in this paragraph
+     */
     public Sentence[] getValue() {
         return this.value;
     }
 
+    /**
+     * Returns a string representation of this {@code Paragraph}.
+     * Sentences are separated by a single space in the output.
+     *
+     * @return the string representation of the paragraph
+     */
     @Override
     public String toString() {
         StringBuffer sb = new StringBuffer();
